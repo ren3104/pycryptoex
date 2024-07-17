16843a6793aa53c84a96a396628ed9840c3d4f1d
ren3104 <2ren3104@gmail.com>
2024-07-17 19:50:46 +0300

    Add proper closing to BaseExchange

--- pycryptoex/base/exchange.py
+++ pycryptoex/base/exchange.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from aiohttp import ClientSession
 
+import asyncio
 from urllib.parse import urlparse
 import sys
 from typing import TYPE_CHECKING
@@ -108,3 +109,7 @@ class BaseExchange:
     
     async def __aexit__(self, *args: Any) -> None:
         await self._session.close()
+
+        # Wait 250 ms for the underlying SSL connections to close
+        # https://docs.aiohttp.org/en/stable/client_advanced.html#graceful-shutdown
+        await asyncio.sleep(0.25)
