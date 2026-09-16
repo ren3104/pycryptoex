61d9b1b6a491b976f33d1dfee03cf62e2ea70b7a
Ren3104 <2ren3104@gmail.com>
2026-09-16 10:52:49 +0300

    feat: add number_to_precision function to utils.py

--- pycryptoex/base/utils.py
+++ pycryptoex/base/utils.py
@@ -5,6 +5,7 @@ import hmac
 import hashlib
 from base64 import b64encode
 import json
+from decimal import Decimal, ROUND_HALF_EVEN
 from typing import TYPE_CHECKING
 
 try:
@@ -23,11 +24,24 @@ except ModuleNotFoundError:
     HAS_CRYPTO = False
 
 if TYPE_CHECKING:
+    from numbers import Number
     from typing import Any
 
 
 def current_timestamp() -> int:
     return int(time.time() * 1000)
+
+
+def number_to_precision(
+    number: Number,
+    precision: str,
+    rounding_mode: str = ROUND_HALF_EVEN
+) -> str:
+    decimal_number = number if isinstance(number, Decimal) else Decimal(str(number))
+    return str(decimal_number.quantize(
+        Decimal(precision),
+        rounding_mode
+    ).normalize())
 
 
 if HAS_ORJSON:
@@ -64,14 +78,14 @@ if HAS_CRYPTO:
 
     def rsa_signature(key: Any, msg: str) -> str:
         if not isinstance(key, rsa.RSAPrivateKey):
-            TypeError(f"expected RSAPrivateKey, got {type(key).__name__}")
+            raise TypeError(f"expected RSAPrivateKey, got {type(key).__name__}")
         return b64encode(
             key.sign(msg.encode("utf-8"), padding.PKCS1v15(), hashes.SHA256())
         ).decode("utf-8")
 
     def eddsa_signature(key: Any, msg: str) -> str:
         if not isinstance(key, ed25519.Ed25519PrivateKey):
-            TypeError(f"expected Ed25519PrivateKey, got {type(key).__name__}")
+            raise TypeError(f"expected Ed25519PrivateKey, got {type(key).__name__}")
         return b64encode(key.sign(msg.encode("utf-8"))).decode("utf-8")
 else:
 
